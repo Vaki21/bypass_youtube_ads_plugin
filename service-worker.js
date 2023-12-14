ac19ce091113938aecc3eed6aee3e4cfac1fc179
Vaki21 <85845371+Vaki21@users.noreply.github.com>
2023-12-14 23:13:05 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,13 @@
 // To handle youtube video page
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-    chrome.tabs.executeScript(null,{file:"popup.js"});
+    //chrome.tabs.executeScript(null,{file:"popup.js"});
+    injectScript;
 });
+
+async function injectScript() {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    await chrome.scripting.executeScript({
+      target: {tabId: tab.id},
+      files: ['popup.js']
+    });
+}
